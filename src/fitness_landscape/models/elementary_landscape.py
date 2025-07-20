49cb934d9bf05942d4dc0650805d247cdfecc82f
MA-Spence <matthew.arthur.spence@gmail.com>
2025-07-20 14:23:56 +1000

    Update to landscape models

--- src/fitness_landscape/models/elementary_landscape.py
+++ src/fitness_landscape/models/elementary_landscape.py
@@ -1,7 +1,8 @@
-from typing import Optional, List
+from typing import Optional, List, Literal
+import numpy as np
 from ..core.landscape import FitnessLandscape
-from ..core.sequence import BaseNumpySequence
-from ..core.graph import create_knn_graph
+from ..core.sequence import BaseNumpySequence, generate_sequences
+from ..core.graph import create_knn_graph, create_hamming_graph
 from ..analysis.eigenmode import eigenmode_decomposition
 
 class ElementaryFitnessLandscape(FitnessLandscape):
@@ -16,24 +17,37 @@ class ElementaryFitnessLandscape(FitnessLandscape):
     """
 
     def __init__(self,
-                 sequences: List[BaseNumpySequence],
                  j: int,
-                 k: int,
+                 N: int = None,
+                 sequences: List[BaseNumpySequence] = None,
                  seed: Optional[int] = None,
-                 graph_type: str = 'knn',
+                 alphabet: List = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'],
+                 graph_type: Literal['knn', 'hamming'] = 'hamming',
                  **kwargs):
 
-        graph = create_knn_graph(sequences=sequences,
-                                 k=k)
+        if sequences is None and N is None:
+            raise ValueError("Either `sequences` or `N` must be provided.")
+    
+
+        if graph_type == 'knn':
+            if sequences is None:
+                raise ValueError("`sequences` must be provided for kNN graph.")
+            graph = create_knn_graph(sequences=sequences,
+                                    k=int(np.sqrt(len(sequences))))
+        
+        elif graph_type == 'hamming':
+            if N is None:
+                raise ValueError("`N` must be provided for Hamming graph.")
+            sequences = generate_sequences(length=N, alphabet=alphabet)
+            graph = create_hamming_graph(sequences=sequences)
+        
         _, eigenvectors = eigenmode_decomposition(graph=graph,
                                                   matrix='laplacian',
                                                   backend='numpy')
-        
         fitness_signal = eigenvectors[:, j]
         for i, node in enumerate(graph.nodes()):
             graph.nodes[node]['fitness'] = fitness_signal[i]
         
-        self.k = k
         self.eigenvector_index = j
         self.seed = seed
 
