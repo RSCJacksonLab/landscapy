665c0ec459bc9ade5c19b3b48721df9a529def08
MA-Spence <matthew.arthur.spence@gmail.com>
2025-08-03 18:35:08 +1000

    Refactored eigenmode to transforms, debugged rndm wlk

--- src/fitness_landscape/transforms/__init__.py
+++ src/fitness_landscape/transforms/__init__.py
@@ -9,11 +9,16 @@ from .graph_fourier import (
     inverse_graph_fourier_transform,
 )
 
-
+from .eigenmode import (
+    eigenmode_decomposition,
+    reconstruct_from_eigenmodes
+)
 __all__ = [
     'walsh_transform',
     'inverse_walsh_transform',
     'walsh_coefficients',
     'graph_fourier_transform',
     'inverse_graph_fourier_transform',
+    'eigenmode_decomposition',
+    'reconstruct_from_eigenmodes',
 ]
